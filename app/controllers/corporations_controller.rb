b486d49849e7ba0004d3d7cb483ed5d39ca9b254
Karl Dockery II <k.dockz@gmail.com>
2010-09-06 01:40:10 -0400

    Company show pages are now viewable by everyone.

--- app/controllers/corporations_controller.rb
+++ app/controllers/corporations_controller.rb
@@ -1,5 +1,5 @@
 class CorporationsController < ApplicationController
-  before_filter :login_required, :except => [:index] 
+  before_filter :login_required, :except => [:index, :show] 
   def index
     @corporations = Corporation.all
   end
